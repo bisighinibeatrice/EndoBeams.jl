79002bb632eda43507590ff5f1dd8f7404c3b976
Baptiste PIERRAT <pierrat@emse.fr>
2021-11-25 10:02:55 +0000

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -6,11 +6,11 @@ DocMeta.setdocmeta!(EndoBeams, :DocTestSetup, :(using EndoBeams); recursive=true
 makedocs(;
     modules=[EndoBeams],
     authors="Baptiste Pierrat",
-    repo="https://gitlab.emse.fr/pierrat/EndoBeams.jl/blob/{commit}{path}#{line}",
+    repo="https://pierrat.pages.emse.fr/EndoBeams.jl/blob/{commit}{path}#{line}",
     sitename="EndoBeams.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://pierrat.gitlab.emse.fr/EndoBeams.jl",
+        canonical="https://pierrat.pages.emse.fr/EndoBeams.jl",
         assets=String[],
         disable_git=true
     ),
